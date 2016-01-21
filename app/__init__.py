d13e708182098e338238a59d39f65125cfe8d5c0
m.gatchalian <m.gatchalian@github.com>
2016-01-21 12:30:21 +0800

    Database Files

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask 
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask (__name__) 
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views 
+from app import views, models
 
 # L01: Creates the application object of flask 
 # L03: App variable gets assigned to flask instance.
--- config.py
+++ config.py
@@ -8,7 +8,16 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 # These are the settings that the Flast-WTF extension needs.
 # L01: Activates the cross-site forgery prevention.
 # L02: This is only needed if the CRSF is enabled.
 #	|--It is used to create a cryptogryphic token that is used to validate a form.
+# L14: Path of database file 
+# L15: Store the SQLAlchemy-migrate data files.
+
